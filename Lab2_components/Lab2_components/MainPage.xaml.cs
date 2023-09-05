aac7ffcb6073fdd4714e7ff0754bbb2a1aaf3813
yuliayuste <yuliashelenzhik@qq.com>
2023-09-05 13:39:57 +0200

    Initial commit

---
+++ Lab2_components/Lab2_components/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab2_components
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
